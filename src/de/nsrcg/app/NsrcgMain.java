bb92055fc55acd6b29ad3404c0827bda50ba20b3
=OMA= Whisler <doc_whisler@yahoo.de>
2016-05-22 22:27:19 +0200

    Projekt init

---
+++ src/de/nsrcg/app/NsrcgMain.java
@@ -0,0 +1,34 @@
+package de.nsrcg.app;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class NsrcgMain extends Application {
+
+	private Stage primaryStage;
+	private Pane mainLayout;
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("NSRCG 5.0");
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(NsrcgMain.class.getResource("view/NsrcgMainView.fxml"));
+		this.mainLayout = loader.load();
+		
+		Scene scene = new Scene(mainLayout);
+		this.primaryStage.setScene(scene);
+		this.primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
