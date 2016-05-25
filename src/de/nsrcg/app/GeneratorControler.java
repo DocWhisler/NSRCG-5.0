5fe5f8ad7c71d61c98de848cb9e194f3cd0ecb2a
=OMA= Whisler <doc_whisler@yahoo.de>
2016-05-25 18:20:05 +0200

    Packagename angepasst

--- src/de/nsrcg/app/GeneratorControler.java
+++ src/de/nsrcg/app/GeneratorControler.java
@@ -1,22 +1,24 @@
 package de.nsrcg.app;
 
 import de.nsrcg.app.constances.CharacterGender;
-import de.nsrcg.app.mobels.shadowrun5.SRCharacter;
+import de.nsrcg.app.models.shadowrun5.SRCharacter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class GeneratorControler {
 	private SRCharacter currentCharacter = new SRCharacter();
     
     @FXML private RadioButton radioMale;
     @FXML private RadioButton radioFemale;
+    @FXML private ToggleGroup geschlecht;
     @FXML private TextField firstName;
     @FXML private TextField familyName;
     @FXML private TextField streetName;
 	
-    @FXML protected void nextButton(ActionEvent event) {
+    @FXML protected void nextButton(ActionEvent event) {    	
     	if(radioMale.isSelected()) {
     		currentCharacter.setCharGender(CharacterGender.male);
     	} else {
@@ -28,7 +30,5 @@ public class GeneratorControler {
     	currentCharacter.setStreetName(streetName.getText());
     	
         currentCharacter.show();
-    }
-
-    
+    }    
 }
