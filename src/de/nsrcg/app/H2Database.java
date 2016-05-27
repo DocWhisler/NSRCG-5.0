775fa9c9b316a4935a5dca8f85f8a863f5d4d0de
=OMA= Whisler <doc_whisler@yahoo.de>
2016-05-27 19:42:28 +0200

    Datenbanktest

--- src/de/nsrcg/app/H2Database.java
+++ src/de/nsrcg/app/H2Database.java
@@ -1,6 +1,5 @@
 package de.nsrcg.app;
 
-import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,15 +7,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class H2Database {
-
 	
-//	public H2Database(){
-//		
-//	}
 	
 	public void startConnection() throws ClassNotFoundException, SQLException{
+		
+//		DROP TABLE IF EXISTS TEST;
+//		CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR(255));
+//		INSERT INTO TEST VALUES(1, 'Hello');
+//		INSERT INTO TEST VALUES(2, 'World');
+//		SELECT * FROM TEST ORDER BY ID;
+//		UPDATE TEST SET NAME='Hi' WHERE ID=1;
+//		DELETE FROM TEST WHERE ID=2;
+		
 		Class.forName("org.h2.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "Nsrcg-5.0", "nsrcg");
+		Connection conn = DriverManager.getConnection("jdbc:h2:./resources/nsrcg50", "Nsrcg-5.0", "nsrcg");
 		
 		Statement stmnt = conn.createStatement();
 		ResultSet rs = stmnt.executeQuery("SELECT * FROM TEST");
